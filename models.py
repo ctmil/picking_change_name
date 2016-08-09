46242a90fa7c53604b01314ac7ba3a393bf4aeb2
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-09 17:32:14 -0300

    Added models

---
+++ models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from openerp import models, api, fields, exceptions
+from openerp.exceptions import ValidationError
+from datetime import date
+
+class account_invoice(models.Model):
+	_inherit = "stock.picking"
+
+	@api.multi
+	def picking_change_name(self):
+                return {'type': 'ir.actions.act_window',
+                        'name': 'Cambiar numero de remito',
+                        'res_model': 'picking.change.name',
+			#'res_id': wizard_id.id,
+                        'view_type': 'form',
+                        'view_mode': 'form',
+                        'target': 'new',
+                        'nodestroy': True,
+                        }
