a5627ff8bff7b17e30e2fb014c29cbfd497b7f0a
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-09 17:27:23 -0300

    Added wizard

---
+++ wizard/wizard_model.py
@@ -0,0 +1,20 @@
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm
+from openerp.osv import osv
+import urllib2, httplib, urlparse, gzip, requests, json
+from StringIO import StringIO
+import openerp.addons.decimal_precision as dp
+from datetime import date
+import logging
+import ast
+from openerp import exceptions
+from openerp.exceptions import ValidationError
+
+#Get the logger
+_logger = logging.getLogger(__name__)
+
+class picking_change_name(models.TransientModel):
+        _name = 'picking.change.name'
+
+	name = fields.Char(string='Nuevo nombre')
+
