e2ccd0c0e1eb5bdb1214b18504b5ee928bb8b7c2
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-09 17:24:16 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Stock Picking - Change Name',
+    'category': 'Warehouse',
+    'version': '0.1',
+    'depends': ['stock'],
+    'data': [
+	#'wizard/wizard_view.xml',
+	#'account_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
