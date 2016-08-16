7f6e313fdc3d7d85fc6401ce8a1ab6dfdd143c02
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-16 16:07:17 -0300

    Added change name

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -16,9 +16,16 @@ _logger = logging.getLogger(__name__)
 class picking_change_name(models.TransientModel):
         _name = 'picking.change.name'
 
-	name = fields.Char(string='Nuevo nombre')
+	name = fields.Char(string='Nuevo nombre',required=True)
 
 	@api.multi
 	def confirm_name(self):
 		import pdb;pdb.set_trace()
+		active_id = self.env.context['active_id']
+		picking = self.env['stock.picking'].browse(active_id)
+		if picking:
+			vals = {
+				'name': self.name
+				}
+			return_id = picking.write(vals)
 		return None
